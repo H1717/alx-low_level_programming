a733fc4b509208bc5daadb30067e91283431e515
myUsername <myemail@gmail.com>
2023-03-16 18:07:33 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - main function
+  *
+  * Return: o on success
+  */
+
+int main(void)
+{
+	printf("Size of a char: %lu bytes(s)", sizeof(char))\n;
+	printf("Size of int: %lu bytes(s)", sizeof(int))\n;
+	printf("Size of a long int: %lu bytes(s)", sizeof(long int))\n;
+	printf("Size of a long long int: %lu bytes(s)", sizeof(long long int))\n;
+	printf("Size of a float: %lu bytes(s)", sizeof(float))\n;
+	return (0);
+}
