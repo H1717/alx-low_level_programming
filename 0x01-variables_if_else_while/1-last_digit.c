2f96adf32bfaa3e014a47469646d017da92e7883
myUsername <myemail@gmail.com>
2023-03-17 16:41:48 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,27 +4,30 @@
 #include <stdio.h>
 /* betty style doc for function main goes there */
 /**
-  * main - main function
-  *
-  * Return: always 0
+  * main - prints a text accordimg number
+  * Return: always (Success)
   */
 
 int main(void)
+
 {
-	int n;
-	int m;
+	int n, lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	m = n % 10;
-	if (m > 5)
-		printf("last digit of %d is %d and is greater than 5", n, m);
-	else if  (m == 0)
-		printf("last digit of %d is %d and is 0", n, m);
-	else if (m < 6 && m != 0)
-		printf("last digit of %d is %d and is less than 6 and not 0", n, m);
-	printf("\n");
-
+	lastd = n % 10;
+	if (lastd > 5)
+	{
+	printf("last digit of %d is %d and is greater than 5", n, lastd);
+	}
+	else if  (lastd == 0)
+	{
+	printf("last digit of %d is %d and is 0", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+	printf("last digit of %d is %d and is less than 6 and not 0", n, lastd);
+	}
 	return (0);
 }
