a169dac8ac9c19df59fdcd9e21513c7d536768fb
myUsername <myemail@gmail.com>
2023-04-13 10:25:49 -0700

    task 5

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 /**
  * Auth: Brennan D Baraban
  * Desc: Header file containing prototypes for all functions
