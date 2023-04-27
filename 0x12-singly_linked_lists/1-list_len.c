fed87d741ba8836f0203cef880487e936ad35ab1
myUsername <myemail@gmail.com>
2023-04-27 10:53:15 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * listint_len - gets the number of nodes in a list
-  * @h: singly linked list to print
-  * Return: number of nodes in the list
-  */
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
 size_t list_len(const list_t *h)
 {
-	size_t i;
+ size_t n = 0;
 
-	for (i = 0; h; i++)
-		h = h->next;
-	return (i);
+ while (h)
+ {
+ n++;
+ h = h->next;
+ }
+ return (n);
 }
