e1aac257944ca1f2852e1d2e32d155bf41688bab
myUsername <myemail@gmail.com>
2023-04-27 11:22:42 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,15 +2,11 @@
 #include "lists.h"
 
 /**
- * list_len - Finds the number of elements in
- *            a linked list_t list.
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- * @h: The linked list_t list.
- * @elements: a counter var for size_t (nodes)
- *
- * Return: The number of elements in h.
- */
+  * listint_len - determines lentgh of linked list
+  * @h: pointer to first node
+  *
+  * Return: size of list
+  */
 
 size_t list_len(const list_t *h)
 {
