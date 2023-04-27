6aaa7e4aba5ec7ddb66008e12b7ca6f5f89400ed
myUsername <myemail@gmail.com>
2023-04-27 11:12:21 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,6 +4,8 @@
 /**
  * list_len - Finds the number of elements in
  *            a linked list_t list.
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
  * @h: The linked list_t list.
  * @elements: a counter var for size_t (nodes)
  *
